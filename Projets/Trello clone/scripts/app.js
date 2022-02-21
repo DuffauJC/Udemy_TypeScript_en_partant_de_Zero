c6c0ea02d160d3c22e309b32b502d3e78c4b5045
Jean-Charles <jeancharles.duffau@gmail.com>
2022-02-21 14:57:13 +0100

    Création et ajout de la première partie du projet

---
+++ Projets/Trello clone/scripts/app.js
@@ -0,0 +1,50 @@
+"use strict";
+const itemsContainer = document.querySelectorAll('.items-container');
+let actualContainer, actualBtn, actualUL, actualForm, actualTextInput, actualValidation;
+function addContainerListeners(currentContainer) {
+    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn');
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn');
+    deleteBtnListners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
+}
+itemsContainer.forEach((container) => {
+    addContainerListeners(container);
+});
+function deleteBtnListners(btn) {
+    btn.addEventListener('click', handleContainerDeletion);
+}
+function addItemBtnListeners(btn) {
+    btn.addEventListener('click', handleAddItem);
+}
+function handleContainerDeletion(e) {
+    const btn = e.target;
+    const btnsArray = [...document.querySelectorAll('.delete-container-btn')];
+    const containers = [...document.querySelectorAll('.items-container')];
+    containers[btnsArray.indexOf(btn)].remove();
+}
+function handleAddItem(e) {
+    const btn = e.target;
+    if (actualContainer) {
+        toggleForm(actualBtn, actualForm, false);
+    }
+    setContainerItems(btn);
+    toggleForm(actualBtn, actualForm, true);
+}
+function toggleForm(btn, form, action) {
+    if (!action) {
+        form.style.display = "none";
+        btn.style.display = 'block';
+    }
+    else if (action) {
+        form.style.display = 'block';
+        btn.style.display = 'none';
+    }
+}
+function setContainerItems(btn) {
+    actualBtn = btn;
+    actualContainer = btn.parentElement;
+    actualUL = actualContainer.querySelector('ul');
+    actualForm = actualContainer.querySelector('form');
+    actualTextInput = actualContainer.querySelector('input');
+    actualValidation = actualContainer.querySelector('.validation-msg');
+}
